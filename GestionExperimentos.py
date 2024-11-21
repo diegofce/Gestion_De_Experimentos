57c7d31e1286a93556d9ec3e36f9ea268f8869f5
DiegoFChacon <chacondiego31@gmail.com>
2024-11-20 22:53:26 -0500

    Initial commit

---
+++ GestionExperimentos.py
@@ -0,0 +1,46 @@
+import datetime 
+
+#Atributos: Nombre, Fecha (DD/MM/AAAA), Tipo y resultados numericos
+
+listaExperimentos = [
+    ['Experimento 1','16/11/2024', 'Quimica', [1,2,3,4,5,6,7,8,9,10]],
+]
+
+def agregar_Experimentos(): #funcion para agregar experimentos
+    pass
+
+def mostrar_Experimentos(): #funcion para mostrar experimentos
+    pass
+
+def calcular_Experimentos(): #funcion para calcular Estadisticas promedio maximo y minimo de experimentos
+    pass    
+
+def comparar_Experimentos(): #funcion para comparar experimentos = 2 o mas 
+    pass
+
+def eliminar_Experimentos(): #funcion para eliminar un experimento
+    pass
+
+def generar_Informe(): #funcion para generar informe tipo resumen + Estadisticas
+    pass
+
+def menu(): #funcion para mostrar menu
+    print("|=======>  Menu:  <=======|")
+    print("| 1. Agregar Experimento  |")
+    print("| 2. Mostrar Experimentos |")
+    print("| 3. Calcular Estadisticas|")
+    print("| 4. Comparar Experimentos|")
+    print("| 5. Eliminar Experimento |")
+    print("| 6. Generar Informe      |")
+    print("| 7. Salir                |")
+    print("|=========================|")
+    
+
+def main(): #funcion principal, controla el flujo del sistema
+    menu()
+    pass
+
+main()
+
+
+
